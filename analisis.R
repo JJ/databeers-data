bba5cffdc9ded3fe17f52eec935e364bf4db4703
JJ Merelo <jjmerelo@gmail.com>
2023-11-21 17:32:07 +0100

    Analisis de tickets

--- analisis.R
+++ analisis.R
@@ -7,7 +7,7 @@ tickets <- fromJSON(file="data/tickets.json")
 tickets.df <- data.frame( ID=character(), session=numeric(), talk=numeric())
 
 for (i in 1:length(tickets)) {
- data <- (str_split(tickets[i],"/"))[[1]]
+ data <- (str_split(tickets[[i]]$value,"/"))[[1]]
  
  tickets.df <- rbind(tickets.df,
                      data.frame(ID=data[7],
